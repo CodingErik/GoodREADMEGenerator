181a436e9baff8b351005aa97f67a72f181f7d6a
Erik De Luna <codingerik1.gmail.com>
2020-06-24 13:36:49 -0500

    updated table of contents

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -15,11 +15,13 @@ function generateMarkdown(data) {
   
   If your README is very long, add a table of contents to make it easy for users to find what they need.
   
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Credits](#credits)
-  * [License](#license)
-  
+  * [Installation](#-Installation)
+  * [Usage](#-Usage)
+  * [Credits](#-Credits)
+  * [License](#-License)
+  * [Badges](#-Badges)
+  * [Contributing](#-Contributing)
+  * [Tests](#-Tests)
   
   ## Installation
   
@@ -30,7 +32,7 @@ function generateMarkdown(data) {
   
   ${data.Usage} 
   
-  Provide instructions and examples for use. Include screenshots as needed. 
+  Include screenshots as needed. 
   
   
   ## Credits
@@ -47,20 +49,19 @@ function generateMarkdown(data) {
   
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
   
-  Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  [shields.io](https://shields.io/)
   
   
   ## Contributing 
   
   ${data.contributing}
   
-  If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+  [Contributor Covenant](https://www.contributor-covenant.org/)
   
   ## Test
   
   ${data.test}
   
-  Go the extra mile and write tests for your application. Then provide examples on how to run them.
   
 
   
